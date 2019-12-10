50c219911879df078cdd7da46b7887f336f4b235
Mzwandile Mahlangu <mzwandile.mahlangu@entelect.co.za>
2019-12-10 21:01:42 +0200

    rediret to not found and added a wildd card

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,16 +12,19 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'not-found', component: PageNotFoundComponent},
     {path: 'users', component: UsersComponent, children: [
       {path: ':id/:name', component: UserComponent},
     ]},
     {path: 'servers', component: ServersComponent, children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent},
-    ]}
+    ]},
+    {path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
@@ -32,7 +35,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
